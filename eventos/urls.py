dc31a2dfa233b0d79a359e3c26454de958f885ad
Heloisa Abrantes <heloisa.abrantesdev@gmail.com>
2026-07-20 12:09:56 -0300

    feat: atualiza rotas para listar eventos e detalhes do evento

--- eventos/urls.py
+++ eventos/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
+    path("", views.listar_eventos, name="listar_eventos"),
+    path("eventos/<int:evento_id>/", views.detalhes_evento, name="detalhes_evento")
 ]
