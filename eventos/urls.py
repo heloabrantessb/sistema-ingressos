d3ebb7bf01b34ac50e3b0d4185ad615413c027db
Heloisa Abrantes <heloisa.abrantesdev@gmail.com>
2026-07-20 12:27:10 -0300

    feat: adiciona templates base e lista de eventos com funcionalidades de compra

--- eventos/urls.py
+++ eventos/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.listar_eventos, name="listar_eventos"),
-    path("eventos/<int:evento_id>/", views.detalhes_evento, name="detalhes_evento")
+    path("eventos/listar", views.listar_eventos, name="listar_eventos"),
+    path("eventos/<int:evento_id>/", views.detalhes_evento, name="detalhes_evento"),
+    path("eventos/<int:evento_id>/comprar/", views.comprar_ingresso, name="comprar_ingresso")
 ]
--- eventos/views.py
+++ eventos/views.py
@@ -1,15 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
-from .models import Evento
+from .models import Evento, Ingresso
 
 def listar_eventos(request):
     eventos = Evento.objects.all()
-    return render(request, 'eventos/', {'eventos': eventos})
+    return render(request, 'eventos/lista.html', {'eventos': eventos})
 
 def detalhes_evento(request, evento_id):
     evento = Evento.objects.get(id=evento_id)
     return render(request, 'eventos/detalhes_evento.html', {'evento': evento})
 
 def comprar_ingresso(request, evento_id):
-    evento = Evento.objects.get(id=evento_id)
-    
+    evento = get_object_or_404(Evento, id=evento_id)
+    if evento.estoque > 0:
+        evento.estoque -= 1
+        evento.save()
+
+        ingresso = Ingresso(evento=evento, comprador=request.user)
+        ingresso.save()
+        
+        return HttpResponse("Ingresso comprado com sucesso!")
