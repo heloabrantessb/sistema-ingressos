db131664e08d3d6914835efdc74be588904268d7
heloabrantessb <heloisa.abrantesdev@gmail.com>
2026-07-24 14:20:00 -0300

    feat: adiciona edição inline de tipos de ingresso no painel administrativo de eventos

--- eventos/admin.py
+++ eventos/admin.py
@@ -2,7 +2,18 @@ from django.contrib import admin
 
 from eventos.models import Evento, TipoIngresso, Comprador, Pedido, Ingresso
 
-admin.site.register(Evento)
+class TipoIngressoInline(admin.StackedInline):
+    model = TipoIngresso
+    extra = 0
+
+class EventoAdmin(admin.ModelAdmin):
+    inlines = [TipoIngressoInline]
+    # list_display = ('nome', 'data', 'local', 'status')
+    # list_filter = ('data', 'status')
+    # search_fields = ('nome', 'local')
+    # ordering = ('data',)
+    
+admin.site.register(Evento, EventoAdmin)
 admin.site.register(TipoIngresso)
 admin.site.register(Comprador)
 admin.site.register(Pedido)
